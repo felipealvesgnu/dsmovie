8d0693e8034de447bb0c7d4b8de88d5a64a3087a
felipealvesgnu <felipealves.gnu@gmail.com>
2022-01-14 21:18:45 -0300

    First request

---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"; // Caso a var nao esteja definida usar o localhost:8080 - variavel REACT_APP_BACKEND_URL padrao do netlify
