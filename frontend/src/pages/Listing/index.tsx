de7eae077ebe0483439a4498f227743210450358
felipealvesgnu <felipealves.gnu@gmail.com>
2022-01-15 09:52:06 -0300

    React useState

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
+import { useState } from 'react';
+import { MoviePage } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 
 function Listing() {
-  // FORMA errada
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  // FORMA errada - pelo ciclo de vida do React, está executando 2 vezesw
   axios.get(`${BASE_URL}/movies?size=12`).then((response) => {
-    console.log(response.data);
+    const data = response.data as MoviePage;
+    setPageNumber(data.number);
   });
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
-
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
